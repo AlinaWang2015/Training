531adfd5d770af68aa93c0e2b0210ed025f10d65
Ken <kenhuang@thinkgeo.com>
2016-07-22 10:29:47 +0800

    change div's class name to news_item

--- Training/js/News/news.js
+++ Training/js/News/news.js
@@ -1,18 +1,18 @@
 ﻿News.news = News.Object.extend({
     statics: {
         add: function (data) {
-            var col12Div = document.createElement('div');
-            col12Div.className = 'col-md-12 column';
+            var newsDiv = document.createElement('div');
+            newsDiv.className = 'news_item';
 
             var titleH2 = document.createElement('h2');
             titleH2.className = 'news_title';
             titleH2.innerHTML = data.date + ' ---- ' + data.title;
-            col12Div.appendChild(titleH2);
+            newsDiv.appendChild(titleH2);
             
             var captionP = document.createElement('p');
             captionP.className = 'news_caption';
             captionP.innerHTML = data.caption;
-            col12Div.appendChild(captionP);
+            newsDiv.appendChild(captionP);
 
             var detailP = document.createElement('p');
             detailP.className = 'view_details';
@@ -21,15 +21,15 @@
             detailA.href = 'thinkgeo.com/press-releases' + data.href;
             detailA.innerHTML = '更多信息';
             detailP.appendChild(detailA);
-            col12Div.appendChild(detailP);
+            newsDiv.appendChild(detailP);
 
             var hr = document.createElement('hr');
             hr.className = 'hr_inner';
-            col12Div.appendChild(hr);
+            newsDiv.appendChild(hr);
 
             var clearDiv = document.createElement('div');
             clearDiv.className = 'row clearfix';
-            clearDiv.appendChild(col12Div);
+            clearDiv.appendChild(newsDiv);
 
             var containerDiv = document.getElementById('News_container');
             containerDiv.appendChild(clearDiv);

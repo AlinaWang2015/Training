593266b22f6239f7afcb3b95024e3887e7dcef79
yudidi <972656027@qq.com>
2016-07-22 10:02:12 +0800

    move inner css and js to outter Folder.

---
+++ Training/js/Index/index.js
@@ -0,0 +1,15 @@
+﻿window.onload = function () {
+
+    //#change img by change img's src
+    $("#pages .page").click(function () {
+        var chooseId = $(this).attr("value");
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+        $("#slides img").attr("src", '../img/Index/head' + chooseId + '.jpg');
+    });
+
+    //# use a timer to switch the slide
+
+    //#change img by fadein fadeout
+
+}

31173d5ca0d4f194ed6597f52a38cf037305a641
yudidi <972656027@qq.com>
2016-07-25 09:31:57 +0800

    delete redundant code called index.*

--- Training/js/Index/index.js
+++ Training/js/Index/index.js
@@ -1,36 +1,17 @@
 ﻿window.onload = function () {
-    var $nowIndex = 0;
-    var $nextIndex = 0;
-
     //# click to change img 
     $("#pages .page").click(function () {
         var clickId = $(this).attr("value");
 
         $(this).addClass("active").siblings().removeClass("active");
         $("#slides").find("img").eq(clickId).addClass("active").siblings().removeClass("active");
-
     });
-
     //use a timer to switch the slide
     var atime = setInterval(function () {
-        if ($("#pages div.active").attr("value") == $("#pages div").last().attr("value")) {
-            $("#pages div").first().click();
+        if ($("#pages .page.active").attr("value") == $("#pages .page").last().attr("value")) {
+            $("#pages .page").first().click();
         } else {
-            $("#pages div.active").next().click();
+            $("#pages .page.active").next().click();
         }
     }, 2000);
-
-    //# change img by animate
 }
-
-//$(document).ready(function () {
-//    $(window).resize(function () {
-//        var widthNow;
-//        var heightNow;
-       
-//        widthNow = $("#slides img.active").attr("width");
-//        alert(widthNow);
-
-//    });
-
-//});

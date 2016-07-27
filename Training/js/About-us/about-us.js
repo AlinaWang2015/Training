2985168e3412887a11ca97420f508c740c7f2fbf
Alilna Wang <wangpengxia2015@outlook.com>
2016-07-27 16:48:35 +0800

    chage about page

--- Training/js/About-us/about-us.js
+++ Training/js/About-us/about-us.js
@@ -1,9 +1,13 @@
 ﻿(function addEventHandler() {
     document.getElementById('ch').addEventListener("click", clickhandler);
     document.getElementById('am').addEventListener("click", clickhandler);
+
+    var arrow1 = document.getElementById("arrow1");
+    arrow1.style.transform = "rotate(45deg)";
+    arrow1.style.transition = "all .5s ease-in .1s";
 })();
 
-var selected = 0;
+var selected = 1;
 function clickhandler(e) {
     if (e.currentTarget.id == 'ch' && selected != 1) {
         var arrow1 = document.getElementById("arrow1");

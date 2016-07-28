6c87817b25b60958eeefe575bf6845241799888d
Piao-Yang <https://github.com/Piao-Yang>
2016-07-28 14:04:15 +0800

    index.css

---
+++ Training/js/SmallScreen.js
@@ -0,0 +1,21 @@
+﻿var width = document.body.clientWidth;
+
+if (width < 768)
+{
+    /*$(".hide_sm").removeAttr("src").addClass("img_sm");*/
+
+    $(".hide_img1").remove();
+    $("#append1").append('<p><strong>Developer&nbsp;Tools</strong><br />ThinkGeo\'s Map Suite GIS map controls and components add interactive customized maps and GIS features to the websites and applications you build with the Microsoft.NET and Xamarin Frameworks. As a software developer, you can use Map Suite\'s powerful map API to add geospatial functionality to your projects, including geocoding,spatial analysis, routing, geoprocessing, and much more using C# and VB.NET.</P>');
+    $(".hide_img2").remove();
+    $("#append2").append('<p><strong>Clound&nbsp;Services</strong><br />Thinkgeo\'s clound services include vector data, image map display, path analysis to select the best path, address location and altitude query.</p>')
+    $(".hide_img3").remove();
+    $("#append3").append('<p><strong>Professional&nbsp;Services</strong><br />Do you need to create an application that uses maps, but aren\'t sure where to begin? ThinkGeo\'s project design, development, implementation and training services will help you bring your GIS ideas to market quickly and affordably. If you\'re interested in having us work with you to create the perfect mapping solution, let us know your project requirements by submitting the form at the bottom of the page.<p>')
+    $(".hide_img4").remove();
+    $("#append4").append('<p><strong>Emergency&nbsp;Support</strong><br />Let us help you maximize the performance of your Map Suite projects by reviewing and analyzing your application and recommending performance-boosting techniques that will improve user experience<P>')
+    $(".hide_img5").remove();
+    $("#append5").append('<p><strong>Grapecity&nbsp;企业文化视频</strong><br />')
+    $(".hide_img6").remove();
+    $("#append6").append('<p><strong>工作机会</strong><br />')
+    $(".hide_img7").remove();
+    $("#append7").append('<p><strong>研发环境</strong><br />')
+}

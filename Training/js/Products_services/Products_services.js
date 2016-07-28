0a4ea01090dc486e7ae8dc506cfb463d37c7e6c6
marco110 <marcoma@thinkgeo.com>
2016-07-28 11:53:34 +0800

    Do some changes.

---
+++ Training/js/Products_services/Products_services.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../jquery-1.10.2.js" />
+window.onresize = function () {
+ var body = $("body").width();
+    if (body > 900) {
+        $(".menu li a").css("margin-left", "10px");
+        $(".menu li a").css("margin-right", "10px");
+    }
+    if (body > 815 && body < 900) {
+        $(".menu li a").css("margin-left", "4px");
+        $(".menu li a").css("margin-right", "4px");
+    }
+    else if (body > 768 && body < 815) {
+        $(".menu li a").css("margin-left", "0");
+        $(".menu li a").css("margin-right", "0");
+    }
+}
